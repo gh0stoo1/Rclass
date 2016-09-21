44e305a585567a936e987969c7aee74ee0a5c991
gh0stoo1 <web007@gmail.com>
2016-09-21 16:09:56 -0400

    Class Stuff --- Graphs

---
+++ WorkingFile.R
@@ -0,0 +1,62 @@
+2=3
+2==3
+2>3
+2<=3
+2<3&2>3
+sqrt(4)
+exp(2)
+log(10)
+ceiling(pi)
+floor(pi)
+factorial(5)
+factorial(10)
+choose(1,2)
+choose(4,2)
+choose(2,3)
+choose(6,2)
+A:B
+5:100
+A==5, B==6, A:B
+A<-c(1,2,3,4,5);A
+B<-c(6,7,8,9,10);B
+C<-c(A,B,"george");C
+D<-rep(1,10);D
+E<-rep(c(1,2,3,4),5);E
+G<-rep(c(1,2,3,4), each=5)
+G
+H<-seq(1,10)
+H
+I<-seq(4,20,by=4); I
+L<-seq(10,2, length=7); L
+str(B)
+A<B|A>B
+A<B||A>B
+A<B&A>B
+t(A)
+t(t(A))
+abs(-B/2)
+table(c(A,B/2))
+A(3)
+A[3]
+[1]
+A[1]
+A[1]<-4; A
+matrix()
+mat<-matrix(data)
+mat2<-matrix(1)
+edit()
+edit(mat2)
+mat2<-Last.value
+mat2
+mat2<-edit(mat2)
+mat2
+vec1<-1:4
+vec2<-5:8
+vec3<-9:12
+vec4<-13:16
+vec.mat<-matrix(data=c(vec1, vec2, vec3, vec4))
+foo<-function(x)
+  a<-1; b<-2; c<-A*x+B;
+return(c)
+foo(2)
+                                                                                                                                                            
